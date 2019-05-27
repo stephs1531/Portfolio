8d02cc8fc3e0435c274b0cc9e165c4ea92987d85
Stephanie Sapp <stephs1531@gmail.com>
2019-05-26 21:16:38 -0500

    working on updating images with jquery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,7 @@
-$(window).load(function(){
-    $('#right-side').imagefit(); 
+$("#balm").on("click", function() {
+
+    // event.preventDefault();
+    $('#project-images').prepend('<img id="first-image" src="../images/the_balm/balm_mockup.png" />')
+    // $('#project-images').append('<img class="image" src="../assets/images/the_balm/the_balm_home.jpg" /> <img class="image" src="../assets/images/the_balm/the_balm_product2.jpg" /> <img class="image" src="../assets/images/the_balm/product_detail2.jpg" /> ')
 });
 
