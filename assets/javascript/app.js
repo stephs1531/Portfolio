d3c1bc89ed600e996c6f22a45a879c7137b83193
Stephanie Sapp <stephs1531@gmail.com>
2019-05-26 21:23:11 -0500

    got appending images to work FUCK YES!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 $("#balm").on("click", function() {
+    console.log("hi i've been clicked!");
 
     // event.preventDefault();
-    $('#project-images').prepend('<img id="first-image" src="../images/the_balm/balm_mockup.png" />')
-    // $('#project-images').append('<img class="image" src="../assets/images/the_balm/the_balm_home.jpg" /> <img class="image" src="../assets/images/the_balm/the_balm_product2.jpg" /> <img class="image" src="../assets/images/the_balm/product_detail2.jpg" /> ')
+    $('#project-images').prepend('<img id="first-image" src="assets/images/the_balm/balm_mockup.png" />')
+    $('#project-images').append('<img class="image" src="assets/images/the_balm/the_balm_home.jpg" /> <img class="image" src="assets/images/the_balm/the_balm_product2.jpg" /> <img class="image" src="assets/images/the_balm/product_detail2.jpg" /> ')
 });
 
