17b67675ae70d1c587f51a631d9dd265224e5dd6
Stephanie Sapp <stephs1531@gmail.com>
2019-05-26 21:42:03 -0500

    added images for bowie section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,17 @@
 $("#balm").on("click", function() {
-    console.log("hi i've been clicked!");
-
-    // event.preventDefault();
+    $("#project-images").empty();
     $('#project-images').prepend('<img id="first-image" src="assets/images/the_balm/balm_mockup.png" />')
     $('#project-images').append('<img class="image" src="assets/images/the_balm/the_balm_home.jpg" /> <img class="image" src="assets/images/the_balm/the_balm_product2.jpg" /> <img class="image" src="assets/images/the_balm/product_detail2.jpg" /> ')
 });
 
+$("#spotify").on("click", function() {
+    $("#project-images").empty();
+    $('#project-images').prepend('<img id="first-image" src="assets/images/spotify_bowie/bowie_big.jpg" />')
+    $('#project-images').append('<img class="image" src="assets/images/spotify_bowie/bowie_landingscreen.jpg"/> <img class="image" src="assets/images/spotify_bowie/bowie_frame1.png" /> <img class="image" src="assets/images/spotify_bowie/bowie_frame2.png" /> <img class="image" src="assets/images/spotify_bowie/bowie_frame3.png" /> <img class="image" src="assets/images/spotify_bowie/bowie_frame4.png" /> <img class="image" src="assets/images/spotify_bowie/bowie_frame5.png" /> <img class="image" src="assets/images/spotify_bowie/bowie_exitscreen.jpg" />')
+});
+
+$("#garage").on("click", function() {
+    $("#project-images").empty();
+    $('#project-images').prepend('<img id="first-image" src="assets/images/spotify_bowie/bowie_big.jpg" />')
+    $('#project-images').append('<img class="image" src="assets/images/spotify_bowie/bowie_landingscreen.jpg"/> <img class="image" src="assets/images/spotify_bowie/bowie_frame1.png" /> <img class="image" src="assets/images/spotify_bowie/bowie_frame2.png" /> <img class="image" src="assets/images/spotify_bowie/bowie_frame3.png" /> <img class="image" src="assets/images/spotify_bowie/bowie_frame4.png" /> <img class="image" src="assets/images/spotify_bowie/bowie_frame5.png" /> <img class="image" src="assets/images/spotify_bowie/bowie_exitscreen.jpg" />')
+});
